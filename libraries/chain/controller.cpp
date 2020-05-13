07e60de2e9982e6c6dc2a092b6c622984eadced0
oldcold <vince@eosio.sg>
2020-05-13 16:00:01 +0800

    fix a potential crash in fetching lib.

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -1758,19 +1758,14 @@ struct controller_impl {
       if (!pbft_enabled) return;
 
       if ( pending_pbft_lib ) {
-         //this is a temp solution for getting current lib, should not use anywhere else;
-         auto current_lib = fork_db.get_block_in_current_chain_by_num(head->bft_irreversible_blocknum)->id;
+         auto current_lib_num = std::max(std::max(head->bft_irreversible_blocknum, head->dpos_irreversible_blocknum), snapshot_head_block);
          fork_db.set_bft_irreversible(*pending_pbft_lib);
          if (!replaying) {
-             auto libs_to_be_emitted = vector<block_state_ptr>{};
+             // emit lib signal to a separate channel, plugins should connect to this channel in order to process libs asap.
              auto b = fork_db.get_block(*pending_pbft_lib);
-             while (b->id != current_lib) {
-                 libs_to_be_emitted.emplace_back(b);
+             while (b && b->block_num > current_lib_num) {
+                 emit( self.new_irreversible_block, b );
                  b = fork_db.get_block(b->prev());
-             }
-             while (!libs_to_be_emitted.empty()) {
-                 emit( self.new_irreversible_block, libs_to_be_emitted.back() );
-                 libs_to_be_emitted.pop_back();
              }
          }
 
